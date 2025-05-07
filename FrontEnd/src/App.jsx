65b7b62a8c4ad2c71ca621d397624a1b17198b6c
Dhiraj Dhiman <dhirajdhiman15@gmail.com>
2025-05-07 22:36:09 +0530

    added routes

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,13 +1,30 @@
-import { ToastContainer, toast } from "react-toastify";
+import { Route, Router, Routes } from "react-router-dom";
+
 import "react-toastify/dist/ReactToastify.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Collection from "./pages/Collection";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Product from "./pages/Product";
 
 function App() {
-    const notify = () => toast("Wow! Toast is working!");
-
     return (
         <div>
-            <button onClick={notify}>Show Toast</button>
-            <ToastContainer />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/collection" element={<Collection />} />
+                <Route path="/order" element={<Orders />} />
+                <Route path="/place-order" element={<PlaceOrder />} />
+                <Route path="/loginn" element={<Login />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/product/:productId" element={<Product />} />
+            </Routes>
         </div>
     );
 }
