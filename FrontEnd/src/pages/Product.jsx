d97ce81358afcee45c7d11f89695c58abe0f139d
Dhiraj Dhiman <dhirajdhiman15@gmail.com>
2025-05-21 00:14:02 +0530

    added desc and review section

--- FrontEnd/src/pages/Product.jsx
+++ FrontEnd/src/pages/Product.jsx
@@ -4,6 +4,7 @@ import ProductImages from "../components/ProductImages";
 
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import Title from "../components/Title";
 
 const Product = () => {
     const { productId } = useParams();
@@ -68,6 +69,31 @@ const Product = () => {
                         <p>Easy return and exchange policy within 7 days.</p>
                     </div>
                 </div>
+            </div>
+
+            {/* Description and review Section */}
+            <div class="mt-20">
+                <div class="flex">
+                    <b class="border px-5 py-3 text-sm">Description</b>
+                    <p class="border px-5 py-3 text-sm">Reviews (122)</p>
+                </div>
+                <div class="flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500">
+                    <p>
+                        An e-commerce website is an online platform that facilitates the buying and selling of products or services over the internet. It serves as a virtual marketplace where businesses and individuals can showcase their products,
+                        interact with customers, and conduct transactions without the need for a physical presence. E-commerce websites have gained immense popularity due to their convenience, accessibility, and the global reach they offer.
+                    </p>
+                    <p>
+                        E-commerce websites typically display products or services along with detailed descriptions, images, prices, and any available variations (e.g., sizes, colors). Each product usually has its own dedicated page with relevant
+                        information.
+                    </p>
+                </div>
+            </div>
+
+            <div class="my-24">
+                <div class=" text-center text-3xl py-2">
+                    <Title text1={"RELATED "} text2={"PRODUCTS"} />
+                </div>
+                <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6"></div>
             </div>
         </div>
     ) : (
